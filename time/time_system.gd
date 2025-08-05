77780f77929928c9ae327d9cf8f24a8db3947c9e
vladilin <vlad.ilin@gmail.com>
2025-08-05 18:07:17 +1200

    clock and buttons

--- time/time_system.gd
+++ time/time_system.gd
@@ -3,7 +3,7 @@ class_name TimeSystem extends Node
 signal updated
 
 @export var date_time: DateTime
-@export var ticks_pr_second: int = 60
+@export var ticks_pr_second: int = 1
 
 func  _process(delta: float) -> void:
 	date_time.increase_by_sec(delta * ticks_pr_second)
--- time_ui.gd
+++ time_ui.gd
@@ -3,6 +3,23 @@ extends Control
 @onready var days_label: Label = $DayControl/day
 @onready var time_label: Label = $clock_bg/ClockControl/time
 
+@onready var play_button: TextureButton = $Play_Button
+@onready var ff_button: TextureButton = $FF_Button
+@onready var skip_button: TextureButton = $Skip_Button
+
+@onready var time_system: TimeSystem = $"../../TimeSystem"
+
+
 func _on_time_system_updated(date_time: DateTime) -> void:
 	days_label.text = str(date_time.days)
-	time_label.text = "%02d:%02d" % [date_time.hours, date_time.minutes]
+	time_label.text = "%02d:%02d:%02d" % [date_time.hours, date_time.minutes, date_time.seconds]
+
+
+func _on_play_button_pressed() -> void:
+	time_system.ticks_pr_second = 1
+
+func _on_ff_button_pressed() -> void:
+	time_system.ticks_pr_second = 150
+
+func _on_skip_button_pressed() -> void:
+	time_system.date_time.increase_by_sec(300)
